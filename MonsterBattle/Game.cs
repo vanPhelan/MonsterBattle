79b8dd7494dc5d1e19ba74b20f21f49622d87f70
Van Phelan <vephelan@gmail.com>
2021-08-11 16:40:48 -0500

    Created Game class

---
+++ MonsterBattle/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonsterBattle
+{
+    class Game
+    {
+        public void Run()
+        {
+            
+        }
+    }
+}
---
+++ MonsterBattle/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MonsterBattle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a new instance of Game
+            Game game = new Game();
+
+            //Run the Game
+            game.Run();
+        }
+    }
+}
