07876ac568d4403a5fdf1ae23d164e7860eb67a9
Van Phelan <vephelan@gmail.com>
2021-08-12 10:13:16 -0500

    Added second monster

--- MonsterBattle/Game.cs
+++ MonsterBattle/Game.cs
@@ -8,14 +8,24 @@ namespace MonsterBattle
     {
         public void Run()
         {
-            float monsterAttack = 10;
-            float monsterDefense = 5;
-            float monsterHealth = 20;
+            //Monster 1
+            float monster1Attack = 10;
+            float monster1Defense = 5;
+            float monster1Health = 20;
 
-            float damageTaken = CalculateDamage(monsterAttack, monsterDefense);
-            monsterHealth = monsterHealth - damageTaken;
+            //Monster 2
+            float monster2Attack = 15;
+            float monster2Defense = 10;
+            float monster2Health = 15;
 
-            Console.WriteLine("Monster has taken " + damageTaken + " damage and has " + monsterHealth + " health remaining.");
+            //Monster 1 attacks Monster 2
+            float damageTaken = CalculateDamage(monster1Attack, monster2Defense);
+            monster2Health = monster2Health - damageTaken;
+            Console.WriteLine("Monster 2 has taken " + damageTaken + " damage and has " + monster2Health + " health remaining.");
+
+            //Monster 2 attacks Monster 1
+            monster1Health = monster1Health - CalculateDamage(monster2Attack, monster1Defense);
+            Console.WriteLine("Monster 1 has taken " + damageTaken + " damage and has " + monster1Health + " health remaining.");
         }
 
         float CalculateDamage(float attack, float defense)
