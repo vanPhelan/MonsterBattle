802bdcc8611c573269fdf4fd59093f6cfbb68a5a
Van Phelan <vephelan@gmail.com>
2021-08-12 10:37:57 -0500

    Created Monster struct and added third monster

--- MonsterBattle/Game.cs
+++ MonsterBattle/Game.cs
@@ -4,28 +4,34 @@ using System.Text;
 
 namespace MonsterBattle
 {
+    struct Monster
+    {
+        public float attack;
+        public float defense;
+        public float health;
+    }
+
     class Game
     {
         public void Run()
         {
             //Monster 1
-            float monster1Attack = 10;
-            float monster1Defense = 5;
-            float monster1Health = 20;
+            Monster goblin = new Monster { attack = 10, defense = 5, health = 20 };
 
             //Monster 2
-            float monster2Attack = 15;
-            float monster2Defense = 10;
-            float monster2Health = 15;
+            Monster slime = new Monster { attack = 15, defense = 10, health = 15 };
+
+            //Monster 3
+            Monster skeleton = new Monster { attack = 20, defense = 5, health = 10 };
 
             //Monster 1 attacks Monster 2
-            float damageTaken = CalculateDamage(monster1Attack, monster2Defense);
-            monster2Health = monster2Health - damageTaken;
-            Console.WriteLine("Monster 2 has taken " + damageTaken + " damage and has " + monster2Health + " health remaining.");
+            float damageTaken = CalculateDamage(goblin.attack, slime.defense);
+            slime.health = slime.health - damageTaken;
+            Console.WriteLine("Monster 2 has taken " + damageTaken + " damage and has " + slime.health + " health remaining.");
 
             //Monster 2 attacks Monster 1
-            monster1Health = monster1Health - CalculateDamage(monster2Attack, monster1Defense);
-            Console.WriteLine("Monster 1 has taken " + damageTaken + " damage and has " + monster1Health + " health remaining.");
+            goblin.health = goblin.health - CalculateDamage(slime.attack, goblin.defense);
+            Console.WriteLine("Monster 1 has taken " + damageTaken + " damage and has " + goblin.health + " health remaining.");
         }
 
         float CalculateDamage(float attack, float defense)
